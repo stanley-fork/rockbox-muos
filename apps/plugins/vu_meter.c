13b932cc0856257d91958b78203daaf46113a08d
IncognitoMan <IncognitoMan@users.noreply.github.com>
2025-06-22 18:30:15 -0400

    Initial patches for PortMaster support. Patches to app_build, plugins and battery support

--- apps/plugins/vu_meter.c
+++ apps/plugins/vu_meter.c
@@ -468,6 +468,17 @@
 #define LABEL_MENU      "MENU"
 #define LABEL_VOLUME    "UP/DOWN"
 
+#elif CONFIG_KEYPAD == SDL_PAD
+#define VUMETER_QUIT    BUTTON_BOTTOMRIGHT
+#define VUMETER_HELP    BUTTON_CENTER
+#define VUMETER_MENU    BUTTON_TOPRIGHT
+#define VUMETER_UP      BUTTON_TOPMIDDLE
+#define VUMETER_DOWN    BUTTON_BOTTOMMIDDLE
+#define LABEL_HELP      "A"
+#define LABEL_QUIT      "Y"
+#define LABEL_MENU      "X"
+#define LABEL_VOLUME    "Up/Down"
+
 #else
 #error No keymap defined!
 #endif
